7f78cc9d7cb7f76326d3d71194b04663dc02d545
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-08-28 19:19:07 +0900

    feat(a11y): mount modal background isolation

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { MotionConfig } from 'framer-motion';
 import App from './App';
+import { AppModalIsolation } from './components/AppModalIsolation';
 import { AppScreenAnnouncer } from './components/AppScreenAnnouncer';
 import './styles.css';
 import './fireTaskEffects.css';
@@ -40,6 +41,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MotionConfig reducedMotion="user">
       <App />
+      <AppModalIsolation />
       <AppScreenAnnouncer />
     </MotionConfig>
   </StrictMode>,
